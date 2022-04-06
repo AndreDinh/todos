29e733e0bb599226ad1d86a1f858bf82ac5f333e
Andre Dinh <dinh.andre@yahoo.com>
2022-04-06 14:19:33 -0700

    working restful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Display from './components/display'
+import Display from './components/display';
+import Pratice from './components/pratice';
 function App() {
   return (
     <Display/>
