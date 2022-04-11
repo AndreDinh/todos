051950ed8df9f13c6510e46d71668e8c91154a91
Andre Dinh <dinh.andre@yahoo.com>
2022-04-11 10:56:38 -0700

    added a delete function

--- src/components/card.js
+++ src/components/card.js
@@ -4,36 +4,53 @@ import CardUI from "./cardUI";
 export default function Card() {
   const [todos, addTodos] = useState([]);
   const [isloading, setIsloading] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsloading(true);
-      const res = await fetch(
-        "https://mytodos-eed82-default-rtdb.firebaseio.com/post.json"
-      );
-      const json = await res.json();
-      //convert array of objects
-      const newTodos = [];
-      for (const key in json) {
-        const todo = {
-          id: key,
-          ...json[key],
-        };
-        newTodos.push(todo);
+
+  async function deleteHandler(id) {
+    const response = await fetch(
+      `https://mytodos-eed82-default-rtdb.firebaseio.com/post/${id}.json`,
+      {
+        method: "DELETE",
+        body: JSON.stringify(id),
+        header: {
+          "Content-Type": "application/json",
+        },
       }
-      addTodos(newTodos);
-      console.log(json);
-      setIsloading(false);
-    };
+    );
+    console.log(response);
     fetchData();
-  }, [addTodos]);
+  }
+
+  const fetchData = async () => {
+    setIsloading(true);
+    const res = await fetch(
+      "https://mytodos-eed82-default-rtdb.firebaseio.com/post.json"
+    );
+    const json = await res.json();
+    //convert array of objects
+    const newTodos = [];
+    for (const key in json) {
+      const todo = {
+        id: key,
+        ...json[key],
+      };
+      newTodos.push(todo);
+    }
+    addTodos(newTodos);
+    console.log(json);
+    setIsloading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <CardUI className="form">
       {isloading ? (
         <p>Loadding</p>
       ) : (
         <ul>
-          {todos.map((item) => (
-            <li key={item.id}>
+          {todos.length === 0 ? <p>no todos</p> : todos.map((item, i) => (
+            <li onClick={() => deleteHandler(item.id)} key={item.id}>
               <h2>{item.title}</h2>
               <p>{item.description}</p>
             </li>
